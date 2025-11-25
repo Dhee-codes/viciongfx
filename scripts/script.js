834ba6341ace68892c704f102127b6130bdf03c2
Dhee-codes <divine.e.obiorah@gmail.com>
2025-11-25 08:16:38 +0100

    Add navigation link highlight on scrol

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,18 @@
 const sections = document.querySelectorAll("section");
 const navLinks = document.querySelectorAll(".nav-link");
+
+const io = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      const id = entry.target.id;
+      const link = document.querySelector(`.nav-link[href="#${id}"]`);
+      if (entry.isIntersecting) {
+        navLinks.forEach(l => l.classList.remove("active"));
+        link.classList.add("active");
+      }
+    });
+  },
+  { threshold: 0.6 }
+);
+
+sections.forEach(sec => io.observe(sec));
